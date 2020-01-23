4d25d9257ead23a970af3f88de12fa982a094bde
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-23 22:54:43 +0100

    user.js create

--- controllers/user.js
+++ controllers/user.js
@@ -6,3 +6,17 @@
 
 const admin = require('firebase-admin')
 const _ = require('lodash')
+
+exports.create = async (payload) => {
+  return admin
+  .auth()
+  .createUser({
+    email: payload.email,
+    password: payload.password,
+  })
+  .then(function(userRecord) {
+    return userRecord
+  })
+  .catch(function(error) {
+    return false
+  })
